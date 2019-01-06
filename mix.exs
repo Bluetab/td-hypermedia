572121b019e2d1a875c956d645a797245b868b9e
Tom Crossland <tom.crossland@bluetab.net>
2019-01-06 17:34:48 +0100

    feat: support phoenix 1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdHypermedia.MixProject do
   def project do
     [
       app: :td_hypermedia,
-      version: "0.1.1",
+      version: "2.11.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,7 +21,8 @@ defmodule TdHypermedia.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:phoenix, "~> 1.3.3"},
+      {:phoenix, "~> 1.3 or ~> 1.4"},
+      {:poison, "~> 2.2 or ~> 3.0", optional: true},
       {:gettext, "~> 0.15"},
       {:canada, "~> 1.0.2"}
     ]
