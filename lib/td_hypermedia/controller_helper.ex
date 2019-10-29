84039b7d184ced1a984a16c8064d4d51416a469a
saul.isern <saul.isern@bluetab.net>
2019-10-29 10:40:09 +0100

    refactor: [TD-606] simplify controller helper

--- lib/td_hypermedia/controller_helper.ex
+++ lib/td_hypermedia/controller_helper.ex
@@ -7,28 +7,17 @@ defmodule TdHypermedia.ControllerHelper do
 
   import Canada.Can
 
-  def collection_hypermedia(helper, conn, resource, resource_type) do
-    %Collection{
-      collection_hypermedia: hypermedia(helper, conn, %{}, resource_type),
-      collection: Enum.map(resource, &{&1, hypermedia(helper, conn, &1, [])})
-    }
-  end
+  def hypermedia(helper, conn, resource, resource_types \\ [])
 
-  def hypermedia(helper, conn, resource, nested \\ [])
-
-  def hypermedia(helper, conn, resource, nested) when is_list(resource) do
-    %Collection{
-      collection_hypermedia: hypermedia(helper, conn, %{}),
-      collection:
-        Enum.into(
-          Enum.map(resource, &{&1, hypermedia(helper, conn, &1, nested)}),
-          %{}
-        )
+  def hypermedia(helper, conn, resources, resource_type) when is_list(resources) do
+    %{
+      actions: hypermedia(helper, conn, %{}, resource_type),
+      collection: Enum.map(resources, &{&1, hypermedia(helper, conn, &1)})
     }
   end
 
   def hypermedia(helper, conn, resource, [h | t]) do
-    hypermedia(helper, conn, resource, t) ++ [%{h => hypermedia_nested(h, conn, resource)}]
+    hypermedia(helper, conn, resource, t) ++ [%{h => hypermedia_impl(h, conn, resource)}]
   end
 
   def hypermedia(helper, conn, resource, []) do
@@ -39,31 +28,35 @@ defmodule TdHypermedia.ControllerHelper do
     hypermedia_impl(helper, conn, resource, resource_type)
   end
 
-  defp hypermedia_impl(helper, conn, %{}, resource_type) do
-    current_resource = conn.assigns[:current_resource] || conn.assigns[:current_user]
+  defp hypermedia_impl(helper, conn, resource, resource_type \\ %{})
 
-    conn
-    |> get_routes
-    |> Enum.filter(&(!is_nil(&1.helper)))
-    |> Enum.filter(&String.starts_with?(&1.helper, helper))
-    |> Enum.filter(&has_permission?(current_resource, &1, resource_type))
-    |> Enum.map(&interpolate(&1, resource_type))
-    |> Enum.filter(&(&1.path != nil))
+  defp hypermedia_impl(helper, conn, %{__struct__: _} = resource, resource_type) do
+    hypermedia_impl(helper, conn, struct_to_map(resource), resource_type)
   end
 
-  defp hypermedia_impl(helper, conn, resource)
-
-  defp hypermedia_impl(helper, conn, resource) do
+  defp hypermedia_impl(helper, conn, resource, resource_type) do
     current_resource = conn.assigns[:current_resource] || conn.assigns[:current_user]
+
+    target = entity_with_permissions(resource, resource_type)
 
     conn
     |> get_routes
     |> Enum.filter(&(!is_nil(&1.helper)))
-    |> Enum.filter(&String.starts_with?(&1.helper, helper))
-    |> Enum.filter(&has_permission?(current_resource, &1, resource))
-    |> Enum.map(&interpolate(&1, resource))
+    |> Enum.filter(&(&1.helper == helper))
+    |> Enum.filter(&has_permission?(current_resource, &1, target))
+    |> Enum.map(&interpolate(&1, target))
     |> Enum.filter(&(&1.path != nil))
     |> Enum.filter(&(resource == %{} or (&1.action != "index" and &1.action != "create")))
+  end
+
+  defp entity_with_permissions(resource, resource_type) do
+    case resource_type == %{} do
+      true -> 
+        resource
+      
+      false -> 
+        resource_type
+    end
   end
 
   defp has_permission?(current_resource, %{opts: opts}, resource) do
@@ -121,22 +114,7 @@ defmodule TdHypermedia.ControllerHelper do
       _ -> nil
     end
   end
-
-  defp hypermedia_nested(helper, conn, %{__struct__: _} = resource) do
-    hypermedia_nested(helper, conn, struct_to_map(resource))
-  end
-
-  defp hypermedia_nested(helper, conn, resource) do
-    current_user = conn.assigns[:current_user]
-
-    conn
-    |> get_routes
-    |> Enum.filter(&(&1.helper == helper and &1.opts == :index))
-    |> Enum.filter(&has_permission?(current_user, &1, resource))
-    |> Enum.map(&interpolate(&1, resource))
-    |> Enum.filter(&(&1.path != nil))
-  end
-
+  
   defp struct_to_map(%{__struct__: name} = resource) do
     key =
       name
